4729cd62c39be86b02b9c0fdbf67635a062d62f4
𝔄𝔷𝔲𝔫𝔬 <237433387+chematic@users.noreply.github.com>
2026-07-26 22:44:39 +0200

    Update timer.js

--- api/timer.js
+++ api/timer.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
-    const targetTimestamp = 1788220800;
+    const targetTimestamp = 1785271408;
 
     res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).json({
         targetTime: targetTimestamp,
         serverTime: Math.floor(Date.now() / 1000)
